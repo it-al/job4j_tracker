9ba47c6880cf8d857c41e795d8779378f74f5d92
ITAL <it-al@yandex.ru>
2020-09-24 15:30:09 +0300

    Revert "Реализация меню за счет шаблона стратегия. [#350130]"
    This reverts commit 8679f23b

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -11,10 +11,10 @@ public class StartUITest {
         String[] answers = {"Fix PC"};
         Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
-//        StartUI.createItem(input, tracker);
-//        Item created = tracker.findAll()[0];
-//        Item expected = new Item("Fix PC");
-//        assertThat(created.getName(), is(expected.getName()));
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertThat(created.getName(), is(expected.getName()));
     }
 
     @Test
@@ -26,9 +26,9 @@ public class StartUITest {
                 String.valueOf(item.getId()),
                 "replaced item"
         };
-//        StartUI.editItem(new StubInput(answers), tracker);
-//        Item replaced = tracker.findById(item.getId());
-//        assertThat(replaced.getName(), is("replaced item"));
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
     }
 
     @Test
@@ -39,8 +39,8 @@ public class StartUITest {
         String[] answers = {
                 String.valueOf(item.getId())
         };
-//        StartUI.deteleItem(new StubInput(answers), tracker);
-//        Item deleted = tracker.findById(item.getId());
-//        assertNull(deleted);
+        StartUI.deteleItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertNull(deleted);
     }
 }
